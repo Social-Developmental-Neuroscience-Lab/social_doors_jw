d8429cefac472364baa30a8fe50d83ab1dcd59d5
Jimmy Wyngaarden <tun46412@cla19097.tu.temple.edu>
2021-01-29 09:45:11 -0500

    local changes to L1stats.sh

--- code/L1stats.sh
+++ code/L1stats.sh
@@ -12,8 +12,8 @@
 # Seed-based PPI and Network PPI should follow activation analyses.
 
 # ensure paths are correct irrespective from where user runs the script
-#scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#maindir="$(dirname "$scriptdir")"
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
 
 
 # study-specific inputs
